e053aa479163baf1d5462d4cc800e1c5277af662
Jack Pattison <jepatt16@gmail.com>
2021-06-03 15:40:11 -0700

    more utils

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy.matlib
 import numpy as np
+import os
 
 def get_matrix(f, l): # give it a filename and the sequence length
     f = open(f, 'r')
@@ -13,7 +14,20 @@ def get_matrix(f, l): # give it a filename and the sequence length
     f.close()
     return m # this matrix is very sparse, don't be surprised if it's hard to find non-zero values
 
+# give a sequence (as a filename), interface with LinearPartition to generate a readable file for it
+def make_matrix_file(f):
+    os.system(f'cat {f} | ../LinearPartition/linearpartition --prefix ./prob_matrix-{f}')
+
+
+# combine above 2 functions (hacky patch with file creation and deletion)
+def make_matrix(f, l):
+    make_matrix_file(f)
+    f = f'prob_matrix-{f}_1'
+    m = get_matrix(f, l)
+    os.system(f'rm {f}')
+    return m
+
 
 if __name__ == '__main__':
-    m = get_matrix('../LinearPartition/prob_matrix_1', 989)
+    m = make_matrix('CRW_00774.fa', 989)
     print(m)
